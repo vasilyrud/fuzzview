0e46025035bd7f548c79dc9e227680013765a81f
Vasily Rudchenko <vasilyrud@gmail.com>
2019-03-12 14:19:19 -0400

    Add subdir example files

--- examples/subA/common.c
+++ examples/subA/common.c
@@ -1,6 +1,12 @@
 
 #include "common.h"
 
+static int Y() {
+    printf("__FILE__: %s\n", __FILE__);
+    return 123;
+}
+
 int ZA() {
+    Y();
     return 222;
 }
--- examples/subB/common.c
+++ examples/subB/common.c
@@ -1,6 +1,12 @@
 
 #include "common.h"
 
+static int Y() {
+    printf("__FILE__: %s\n", __FILE__);
+    return 456;
+}
+
 int ZB() {
+    Y();
     return 333;
 }
