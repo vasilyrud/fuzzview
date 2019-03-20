8b0f18754365d8b6e8129d3984bf79add0ff9379
Vasily Rudchenko <vasilyrud@gmail.com>
2019-03-20 19:00:57 -0400

    Use graph nodes for all blocks

--- fuzzview/fuzzview/cfg/grapher.py
+++ fuzzview/fuzzview/cfg/grapher.py
@@ -3,39 +3,6 @@ import json
 
 import fuzzview.const as const
 
-class GraphNode(object):
-
-    def __init__(self, module, func, block):
-        self.module = module
-        self.func = func
-        self.block = block
-
-    def get_dimensions(self):
-        
-        max_num_edges = max(
-            len(self.block['prev']), 
-            len(self.block['next'])
-        )
-        block_width = max(1, max_num_edges)
-
-        block_height = len(self.block['calls']) + 1
-
-        return block_width, block_height
-
-    def __str__(self):
-
-        ret_str = ''
-
-        block_width, block_height = self.get_dimensions()
-        grid = [['.' for j in range(block_width)] for i in range(block_height)]
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                ret_str += '#'
-            ret_str += '\n'
-        
-        return ret_str
-
 class Grapher(object):
 
     def __init__(self, project_src_dir):
@@ -69,30 +36,70 @@ class Grapher(object):
     def _generate_file_graphs(self, module):
 
         for func in self._sorted_funcs(module['functions']):
-            func_grapher = FuncGrapher(module, func)
-            func_grapher.generate_graph()
+            func_graph = FuncGraph(module, func)
 
-class FuncGrapher(object):
+class FuncGraph(object):
 
     def __init__(self, module, func):
         self.module = module
         self.func = func
+        self.nodes = []
+        
+        self._generate_graph()
 
-    def _min_block(self, blocks_obj):
-        return min(
+    def _first_block(self, blocks_obj):
+
+        first_block = min(
             blocks_obj.values(),
             key=lambda f: f['number']
         )
 
-    def generate_graph(self):
-
-        first_block = self._min_block(self.func['blocks'])
         assert not first_block['prev']
+        return first_block
 
-        first_node = GraphNode(self.module, self.func, first_block)
+    def _generate_graph(self):
 
-        block_width, block_height = first_node.get_dimensions()
+        first_block = self._first_block(self.func['blocks'])
 
-        print(self.func['name'] + ':' + first_block['name'])
-        print(str(block_width) + 'x' + str(block_height))
-        print(first_node)
+        for block in self.func['blocks'].values():
+            self.nodes.append(GraphNode(self.module, self.func, block))
+
+        for node in self.nodes:
+            node_width, node_height = node.get_dimensions()
+
+            print(node.func['name'] + ':' + node.block['name'])
+            print(str(node_width) + 'x' + str(node_height))
+            print(node)
+
+class GraphNode(object):
+
+    def __init__(self, module, func, block):
+        self.module = module
+        self.func = func
+        self.block = block
+
+    def get_dimensions(self):
+        
+        max_num_edges = max(
+            len(self.block['prev']), 
+            len(self.block['next'])
+        )
+        block_width = max(1, max_num_edges)
+
+        block_height = len(self.block['calls']) + 1
+
+        return block_width, block_height
+
+    def __str__(self):
+
+        ret_str = ''
+
+        block_width, block_height = self.get_dimensions()
+        grid = [['.' for j in range(block_width)] for i in range(block_height)]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                ret_str += '#'
+            ret_str += '\n'
+        
+        return ret_str
