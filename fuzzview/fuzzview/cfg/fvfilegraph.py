c8bd4d254247978797159fdfa9e9414db43f47a5
Vasily Rudchenko <vasilyrud@gmail.com>
2019-03-21 22:53:35 -0400

    Add longest depths

--- fuzzview/fuzzview/cfg/fvfilegraph.py
+++ fuzzview/fuzzview/cfg/fvfilegraph.py
@@ -25,15 +25,18 @@ class FuncGraph(object):
         self._generate_nodes()
         self._set_back_edges()
         self._set_shortest_depths()
+        self._set_longest_depths()
 
         # Print nodes
         for node in self.nodes.values():
             node_width, node_height = node.get_dimensions()
 
-            print(node.func['name'] + ':' + node.block['name'])
-            print('shortest_depth: ' + str(node.shortest_depth))
-            print(str(node_width) + 'x' + str(node_height))
-            print(node)
+            print(node.func['name'] + ':' + node.block['name'], end=', ')
+            print('shortest_depth: ' + str(node.shortest_depth), end=', ')
+            print('longest_depth: ' + str(node.longest_depth), end=', ')
+            # print(str(node_width) + 'x' + str(node_height))
+            # print(node)
+            print('')
 
     @property
     def first_block(self):
@@ -56,17 +59,31 @@ class FuncGraph(object):
         for src, dest in self.func['back_edges'].items():
             self.nodes[src].back_edges.add(dest)
 
-    def _sorted_next(self, cur_block):
+    def _sorted_forward_next(self, cur_block):
+        # back edges for cur_block
+        back_edge_dests = [
+            dest
+            for src, dest in self.func['back_edges'].items()
+            if src == cur_block['name']
+        ]
+
+        # subtract back edges from next edges
+        forward_next = filter(
+            lambda b_name: b_name not in back_edge_dests, 
+            cur_block['next']
+        )
+
+        # sort all forward edges
         return sorted(
-            cur_block['next'], 
+            forward_next, 
             key=lambda b_name: self.func['blocks'][b_name]['number']
         )
 
     def _set_shortest_depths(self):
         seen = set()
         q = deque()
-        depth_counter = 0
-        q.append((depth_counter, self.first_block))
+        init_depth = 0
+        q.append((init_depth, self.first_block))
 
         while q:
             cur = q.popleft()
@@ -75,19 +92,38 @@ class FuncGraph(object):
             cur_block = cur[1]
             cur_name  = cur_block['name']
             cur_node  = self.nodes[cur_name]
-            depth_counter = cur_depth + 1
 
             # Save the depth in the node
             cur_node.shortest_depth = cur_depth
 
-            for next_block_name in self._sorted_next(cur_block):
+            for next_block_name in self._sorted_forward_next(cur_block):
                 if next_block_name in seen:
                     continue
 
                 seen.add(next_block_name)
                 next_block = self.func['blocks'][next_block_name]
 
-                q.append((depth_counter, next_block))
+                q.append((cur_depth + 1, next_block))
+
+    def _set_longest_depths(self):
+        q = deque()
+        init_depth = 0
+        q.append((init_depth, self.first_block))
+
+        while q:
+            cur = q.popleft()
+
+            cur_depth = cur[0]
+            cur_block = cur[1]
+            cur_name  = cur_block['name']
+            cur_node  = self.nodes[cur_name]
+
+            if cur_depth >= cur_node.longest_depth:
+                cur_node.longest_depth = cur_depth
+            
+            for next_block_name in self._sorted_forward_next(cur_block):
+                next_block = self.func['blocks'][next_block_name]
+                q.append((cur_depth + 1, next_block))
 
 class GraphNode(object):
 
@@ -99,7 +135,7 @@ class GraphNode(object):
         # Filled in by FuncGraph
         self.back_edges = set()
         self.shortest_depth = None
-        self.longest_depth  = None
+        self.longest_depth  = 0
 
     def get_dimensions(self):
         max_num_edges = max(
@@ -109,6 +145,9 @@ class GraphNode(object):
         block_width = max(1, max_num_edges)
 
         block_height = len(self.block['calls']) + 1
+
+        assert block_width  > 0
+        assert block_height > 0
 
         return block_width, block_height
 
