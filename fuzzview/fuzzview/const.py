3e81e3d9244fe65f71c198dd24b1f14f6627efec
Vasily Rudchenko <vasilyrud@gmail.com>
2019-04-20 01:18:43 -0400

    Add apache 2.0 license

--- fuzzview/fuzzview/const.py
+++ fuzzview/fuzzview/const.py
@@ -1,14 +1,31 @@
-# Custom file extensions
+# Copyright 2019 Vasily Rudchenko - Fuzzview
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+''' Custom file extensions 
+'''
 CFG_JSON_EXTENSION = '.cfg.json'
 CFG_DOT_EXTENSION = '.cfg.dot'
 CFG_PDF_EXTENSION = '.cfg.pdf'
 FILE_PNG_EXTENSION = '.file.png'
 FUNC_PNG_EXTENSION = '.func.png'
 
-# Special dir names
+''' Special dir names 
+'''
 FV_FUNC_IMG_DIR = 'fuzzview_function_images'
 
-# Various env vars used by fuzzview
+''' Various env vars used by fuzzview 
+'''
 FV_ENV_VAR = 'FV_DIR'
 LLVM_ENV_VAR = 'FV_LLVM_DIR'
 NICE_JSON_ENV_VAR = 'FV_NICE_JSON'
