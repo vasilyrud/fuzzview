9ca0fcf07f961d29023e11ae50d8c28ce7a37bf5
Vasily Rudchenko <vasilyrud@gmail.com>
2019-04-20 22:05:55 -0400

    Use sudo cmake install

--- travis/install_cmake.sh
+++ travis/install_cmake.sh
@@ -3,5 +3,5 @@
 cd $HOME
 wget https://github.com/Kitware/CMake/releases/download/v3.14.0/cmake-3.14.0-Linux-x86_64.sh
 chmod +x cmake-3.14.0-Linux-x86_64.sh
-bash cmake-3.14.0-Linux-x86_64.sh --prefix=/usr/local --skip-license
+sudo bash cmake-3.14.0-Linux-x86_64.sh --prefix=/usr/local --skip-license
 rm cmake-3.14.0-Linux-x86_64.sh
